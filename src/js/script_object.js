f62d501cf1a1c5eea6a0b5c875a8f138232d27bd
Troian Ruslan <300493rt@gmail.com>
2021-11-15 10:37:28 +0200

    test 1-5 tasks

--- src/js/script_object.js
+++ src/js/script_object.js
@@ -52,14 +52,14 @@ console.log(findTitle(testArr3, 'js'));
 console.log('---countCharacters---');
 
 const countCharacters = (string) => {
-    const arrayFromString = string.split('');
+    const arrayFromString = string.split(' ').join('').split('');
     let result = {};
     arrayFromString.forEach(item => {
         result[item] = (arrayFromString.filter(element => element === item).length);
     })
 
     return result    
-}
+};
 console.log(countCharacters('development by creating an account on GitHub'));
 console.log('---getNextPalindrome---')
 
@@ -78,11 +78,11 @@ const getNextPalindrome = (number) => {
     } else {
         return minPalindrome;
     };
-}
+};
 console.log(getNextPalindrome(9));
 console.log('---set---');
 
-const set = (array) => {
+const getSet = (array) => {
     let userSet = [];
     array.forEach((item) => {
         !userSet.includes(item) ? userSet.push(item) : true;
@@ -95,7 +95,7 @@ const set = (array) => {
         if (!Object.values(userSet).includes(value)) {
         userSet[value] = value;
         } else {
-        return '${value} already in Set';
+        return `${value} already in Set`;
         }
     };
     userSet.has = (value) => {
@@ -105,12 +105,55 @@ const set = (array) => {
         if (Object.values(userSet).includes(value)) {
         delete userSet[value];
         } else {
-        return '${value} does not in Set';
+        return `${value} does not in Set`;
         }
     };
     return userSet;
 }
 
-const newSet = set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+const newSet = getSet([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 newSet.add(10);
 console.log(newSet, newSet.has(10), newSet.remove(10), newSet);
+
+
+// function mySet(array) {
+//   let setName = [];
+
+//   array.forEach((item) => {
+//     !setName.includes(item) ? setName.push(item) : true;
+//   });
+//   setName = setName.reduce((acc, item, index) => {
+//     acc[index + 1] = item;
+//     return acc;
+//   }, {});
+
+//   setName.add = function (value) {
+//     if (!Object.values(setName).includes(value)) {
+//       setName[value] = value;
+//     } else {
+//       return "${value} already in Set";
+//     }
+//   };
+
+//   setName.has = function (value) {
+//     return Object.values(setName).includes(value);
+//   };
+
+//   setName.remove = function (value) {
+//     if (Object.values(setName).includes(value)) {
+//       delete setName[value];
+//     } else {
+//       return "${value} does not in Set";
+//     }
+//   };
+//   return setName;
+// }
+
+// const newSet = mySet([
+//   1, 3, 2, 2, 5, 76, 8, 5, 3, 6, 2, 6, 23, 41, 7, 34, 23, 364, 23, 5236,
+// ]);
+// newSet.add(222);
+// console.log("-> newSet", newSet);
+// console.log("-> newSet.has(222)", newSet.has(222));
+// console.log("-> newSet.remove(222)", newSet.remove(222));
+// console.log("-> newSet", newSet);
--- src/tests/script_object.test.js
+++ src/tests/script_object.test.js
@@ -22,11 +22,54 @@ describe ('Working objects' , () => {
         });
     });
     describe('Algorithm that filters the array given as the first parameter and returns an array of objects that contain the given string as the second parameter in their headers (case insensitive)', () => {
-        it('transfer [1, 3, 5, 7, 4]', () => {
-            expect(getNumber([1, 3, 5, 7, 4])).to.equal(4);
+        const testArr = [{
+            title: 'Some title with Js'
+            }, 
+            {
+            title: 'I like JS'
+            },
+            {
+            user: 'This obj doesnt have key title js'
+            }, 
+            {
+            title: 'C# - is the best!'
+            }];
+        it('transfer testArr, string - js', () => {
+            expect(findTitle(testArr, 'js')).to.eql([{title: 'Some title with Js'}, {title: 'I like JS'}]);
         });
-        it('transfer [2, 4, 5, 8, 8]', () => {
-            expect(getNumber([2, 4, 5, 8, 8])).to.equal(5);
+        it('transfer testArr, string - key, (title: user)', () => {
+            expect(findTitle(testArr, 'key')).to.eql([]);
+        });
+        it('transfer testArr, string - html, (not in array)', () => {
+            expect(findTitle(testArr, 'key')).to.eql([]);
+        });
+    });
+    describe('The function must return an object in which the keys are the characters of the string, the value is the number of repetitions of characters in the string.', () => {
+        it('transfer string  - sparrow', () => {
+            expect(countCharacters('sparrow')).to.eql({s: 1, p: 1, a: 1, r: 2, o: 1, w: 1});
+        });
+        it('transfer string  - bobble', () => {
+            expect(countCharacters('bobble')).to.eql({b: 3, o: 1, l: 1, e: 1});
+        });
+        it('transfer string  - a 2ab !d', () => {
+            expect(countCharacters('a 2ab !d')).to.eql({a: 2, b:1, d:1, 2:1, '!':1});
+        });
+    });
+    describe('The function should find the next larger positive palindrome.', () => {
+        it('transfer namber  - 5', () => {
+            expect(getNextPalindrome(5)).to.equal(11);
+        });
+        it('transfer namber  - 13', () => {
+            expect(getNextPalindrome(13)).to.equal(22);
+        });
+        it('transfer namber  - -1', () => {
+            expect(getNextPalindrome(-1)).to.equal(11);
+        });
+    });
+    describe('Create a Set data structure using an object, create add, remove, has methods.', () => {
+        // const newSet = mySet([1, 3, 2]);
+        it('transfer - 5, new set', () => {
+            expect(getSet.add([1, 3, 2])).to.equal('4 already in Set');
         });
     });
 });
