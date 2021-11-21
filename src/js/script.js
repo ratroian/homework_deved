024c0ba5acc6936aca86d3d3adec2ca31233f910
ratroian <300493rt@gmail.com>
2021-11-21 22:18:30 +0200

    add local storag

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,18 @@ const messageTemplate = document.querySelector('#list-item-template').content
 const contentTemplate = messageTemplate.querySelector('.content');
 const buttonTodoAdd = document.querySelector('.todo-add')
 let messageElement;
+let toDos;
+
+const toLocal = () => {
+    toDos = list.innerHTML
+    localStorage.setItem('todos', toDos)
+};
 
 const showMessageSuccess = () => {
     contentTemplate.textContent = input.value
     messageElement = messageTemplate.cloneNode(true);
     list.appendChild(messageElement);
+    toLocal();
 };
 
 buttonTodoAdd.onclick = function (evt) {
@@ -23,14 +30,22 @@ list.addEventListener('click', (event) => {
     switch (target.id) {
         case ('priority'):
             target.parentNode.classList.toggle('red');
+            toLocal();
             break;
         case ('edit'):
             target.parentNode.contentEditable = true;
+            toLocal();
             break;
         case('remove'):
-            target.parentNode.remove()
+            target.parentNode.remove();
+            toLocal();
             break;
         case ('done'):
-            target.parentNode.classList.toggle('green')
+            target.parentNode.classList.toggle('green');
+            toLocal()
     };
 });
+
+if (localStorage.getItem('todos')) {
+    list.innerHTML = localStorage.getItem('todos')
+}
