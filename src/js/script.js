dd5b6b82d463282281bca41d96e4a53f3d8a0c1b
ratroian <300493rt@gmail.com>
2021-11-28 15:42:28 +0200

    add pagination

--- src/js/script.js
+++ src/js/script.js
@@ -2,29 +2,16 @@
 const imagesContainer = document.querySelector('.images-list');
 const imageTemplate = document.querySelector('#image-template').content;
 const popupTemplate = document.querySelector('#popup-template').content;
-const body = document.querySelector('body')
+const fullImage = popupTemplate.querySelector('.image-full');
+const titleImage = popupTemplate.querySelector('.title')
+const body = document.querySelector('body');
+const pages = document.querySelector('.pages')
 const API = 'https://jsonplaceholder.typicode.com/photos?_limit=250';
 let IMAGES = [];
 let popupElement;
-const obj = {
-    a: 1,
-    b: 2
-}
-console.log(obj.a)
-
-// const renderItems = (items) => {
-//     const listFragment = document.createDocumentFragment();
-//
-//     items.forEach(item => {
-//         const itemT = imageTemplate.cloneNode(true);
-//         itemT.querySelector('.image').src = item.url;
-//         itemT.querySelector('.title').textContent = item.title;
-//         imagesContainer.appendChild(listFragment)
-//     })
-// }
 
 const getData = () => {
-    fetch(API)
+    return fetch(API)
         .then((response) => {
             if (response.ok) {
                 return response;
@@ -37,44 +24,62 @@ const getData = () => {
             console.log(IMAGES)
             return IMAGES
         })
-        .then((items) => {
-    // const listFragment = document.createDocumentFragment();
-
-    items.forEach(item => {
-        // console.log(item.title)
-        const itemT = imageTemplate.cloneNode(true);
-        itemT.querySelector('.image').src = item.thumbnailUrl;
-        console.log(itemT.querySelector('.image'))
-        // itemT.querySelector('.image-full').src = item.url;
-        imagesContainer.appendChild(itemT)
-    })
-})
+        // .then()
         .catch((error) => console.log(error));
 }
 
-console.log(getData());
-console.log(IMAGES);
 
+const renderList = (items) => {
+    imagesContainer.innerHTML = '';
+    items.forEach(item => {
+        const itemTemplate = imageTemplate.cloneNode(true);
+        itemTemplate.querySelector('.image').src = item.thumbnailUrl;
+        itemTemplate.querySelector('.image').dataset.id = item.id;
+        // console.log(itemT.querySelector('.image'))
+        // itemT.querySelector('.image-full').src = item.url;
+        imagesContainer.appendChild(itemTemplate)
+    })
+}
 
-const showMessageSuccess = () => {
+const quantutyItems = 50;
+
+getData().then(() => renderList(IMAGES.slice(0, quantutyItems)));
+
+const getPagination = (event) => {
+    let target = event.target;
+    let page = +target.textContent
+    renderList(IMAGES.slice(quantutyItems* (page-1), quantutyItems* (page-1) + quantutyItems))
+}
+
+pages.addEventListener('click', getPagination)
+
+const showPopup = (event) => {
+    let target = event.target;
+    const idItem = +target.dataset.id;
+    const url = IMAGES.find((item) => item.id === idItem)
+    console.log(url)
+    // console.log(target.dataset.id)
     popupElement = popupTemplate.cloneNode(true);
-
+    popupElement.querySelector('.image-full').src = url.url;
+    //
     body.appendChild(popupElement);
 
-    body.addEventListener('click', messageDeleteHandler, {once: true});
+    // body.addEventListener('click', messageDeleteHandler, {once: true});
 
-    body.addEventListener('keydown', onPopupEscKeydown);
+    // body.addEventListener('keydown', onPopupEscKeydown);
 };
 
-const messageDeleteHandler = () => {
-    popupElement.remove();
-};
+imagesContainer.addEventListener('click', showPopup)
 
-const onPopupEscKeydown = (evt) => {
-    if (isEscEvent(evt)) {
-        messageDeleteHandler();
-        body.removeEventListener('keydown', onPopupEscKeydown);
-    }
-};
+// const messageDeleteHandler = () => {
+//     popupElement.remove();
+// };
+
+// const onPopupEscKeydown = (evt) => {
+//     if (isEscEvent(evt)) {
+//         messageDeleteHandler();
+//         body.removeEventListener('keydown', onPopupEscKeydown);
+//     }
+// };
 
 const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
