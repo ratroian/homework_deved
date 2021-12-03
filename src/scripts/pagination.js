70315631884d7657bf243d9d740994d9b36f07ea
ratroian <300493rt@gmail.com>
2021-12-04 00:33:01 +0200

    edit pagination js

--- src/scripts/pagination.js
+++ src/scripts/pagination.js
@@ -4,7 +4,7 @@ import {renderList} from "./render.js";
 const QUANTITY_ITEMS = 50;
 
 const makePagination = (event) => {
-    let target = event.target;
+    let target = Event;
     let page = +target.textContent;
     renderList(IMAGES.slice(QUANTITY_ITEMS* (page-1), QUANTITY_ITEMS* (page-1) + QUANTITY_ITEMS));
 };
