06345ca63fd5c3f3dc14a9d4859fa98c68545e67
ratroian <300493rt@gmail.com>
2022-01-11 10:13:49 +0200

    es6

---
+++ front/webpack.config.ts
@@ -0,0 +1,43 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config: Configuration = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000,
+  },
+};
+export default config;
